c77b645d0f6820545afe22574f600769497e0183
Stoychev <stoychev.nas@gmail.com>
2024-06-06 19:22:28 +0300

    Create start camera functionality. Save pictures. Find device location.

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,80 @@
+import { useState } from "react";
+
+import { View, Text, Image, Alert, StyleSheet } from "react-native";
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from "expo-image-picker";
+
+import OutlineButton from "../UI/OutlineButton";
+
+import { Colors } from "../../constants/colors";
+
+function ImagePicker() {
+    const [pickedImage, setPickedImage] = useState();
+
+    // For iOS
+    const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;
+        }
+
+        if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+            Alert.alert(
+                'Insufficiant Permissions!',
+                'You need to grant camera permissions to use this app.'
+            );
+            return false;
+        }
+        return true;
+    }
+    // For iOS
+    async function takaImageHandler() {
+        // For iOS
+        const hasPermission = await verifyPermissions();
+        if (!hasPermission) {
+            return;
+        }
+        // For iOS
+        const image = await launchCameraAsync({
+            allowsEditing: true,
+            aspect: [16, 9],
+            quality: 0.5,
+        });
+        setPickedImage(image.assets[0].uri)
+        // setPickedImage(image.uri);
+    }
+
+    let imagePreview = <Text>No image taken yet!</Text>;
+
+    if (pickedImage) {
+        imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+    }
+
+    return (
+        <View>
+            <View style={styles.imagePreview}>{imagePreview}</View>
+            <OutlineButton icon="camera" onPress={takaImageHandler}>Take Image</OutlineButton>
+        </View>
+    )
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        marginVertical: 8,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primary100,
+        borderRadius: 4,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+});
---
+++ util/location.js
@@ -0,0 +1,8 @@
+const GOOGLE_API_KEY='';
+
+export function getMapPreview(lat, lng) {
+    const imagePreview = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=13&size=400x200&maptype=roadmap
+    &markers=color:red%7Clabel:S%7C${lat},${lng}
+    &key=${GOOGLE_API_KEY}&signature=YOUR_SIGNATURE`;
+    return imagePreview;
+}
